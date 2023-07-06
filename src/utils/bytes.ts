0a83f28406f0a8aef3b8f58deb57158a90ae7515
George <bulakh.96@gmail.com>
2023-07-06 23:08:00 +0300

    Fix drng to match plutus

--- src/utils/bytes.ts
+++ src/utils/bytes.ts
@@ -1,8 +1,12 @@
-import { type Hex, toHex } from 'ts-binary-newtypes'
+import { type Hex, toHex, fromHex } from 'ts-binary-newtypes'
 import { BigNumber } from 'bignumber.js';
 import blakejs from 'blakejs'
 import { littleEndianToInteger } from 'ts-practical-fp'
 
 // Calculate n-byte random number from bytestring seed
-export const drng = (byteCount: number) => (byteString: Hex) =>
-   littleEndianToInteger(toHex(blakejs.blake2b(byteString, undefined, 32).slice(32 - byteCount)))
+export const drng = (byteCount: number) => (byteString: Hex) => {
+   if (byteCount > 32 || byteCount < 1) {
+      throw new Error('drng: invalid byteCount')
+   }
+   return littleEndianToInteger(toHex(blakejs.blake2b(fromHex(byteString), undefined, 32).slice(32 - byteCount)))
+}
